ffc8bcf499fbb6b86c136b2e214d3f4a31cd9096
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-10 22:14:56 +0900

    練習

---
+++ app.js
@@ -0,0 +1,34 @@
+// alert('Hello World');
+// let hello = 'Hello World';
+// alert(hello);
+// 足し算
+// alert(4 + 3);
+// // 引き算
+// alert(8 - 5);
+// // 掛け算
+// alert(2 * 6);
+// // 割り算
+// alert(10 / 2);
+// alert('Hello' + 'World');
+// let str1 = 'Hello';
+// let str2 = 'World!!';
+// alert(str1 + str2);
+// let orange = 100;
+// let apple = 120;
+
+// if(orange > apple){
+//     alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//     alert('みかんとりんごの値段が同じ値段');
+// } else{
+//     alert('みかんの値段がりんごより高い');
+// }
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+    hum = num * 2;
+    count = count + 1;
+}
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
