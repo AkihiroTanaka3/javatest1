094c47738b9b816e248fd8bb7e4bf71f368e0df2
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-11 20:39:10 +0900

    追加

--- app.js
+++ app.js
@@ -23,12 +23,19 @@
 // } else{
 //     alert('みかんの値段がりんごより高い');
 // }
-let max = 100;
-let num = 1;
-let count = 0;
+// let max = 100;
+// let num = 1;
+// let count = 0;
 
-while(num < max){
-    hum = num * 2;
-    count = count + 1;
+// while(num < max){
+//     num = num * 2;
+//     count = count + 1;
+// }
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+    num = num + i;
 }
-alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+alert('1から10まで足し算した結果は' + num + 'です');
