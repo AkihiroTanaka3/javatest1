8a0e28c1876936e1fc36ef46a452d432794ea536
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-12 15:01:23 +0900

    追加

--- practice-variable/sample.js
+++ practice-variable/sample.js
@@ -1,4 +1,35 @@
-var nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// function foo () {
+//     let x = "webcamp"
+//     {
+//         let y = "hello webcamp"
+//     }
+//     console.log(x)
+//     console.log(y)
+// }
+
+// foo()
+
+// for (let i = 0; i < 10; i++) {     
+//     console.log(i) 
+//   }
+  
+//   console.log(i)
+
+// 変数の巻き上げ(ホイルスティング)注意
+var str = "webcamp"
+
+function foo() {
+    console.log(str)
+    var str = "dmm webcamp"
+    console.log(str)
+}
+
+foo()
+// varはあまり使われない
+// letは再代入が可能、constは再代入が不可能。
+// letを使っていると、どこかで値が変わってないかをチェックする際、全てのコードを確認する必要が出てくる。
